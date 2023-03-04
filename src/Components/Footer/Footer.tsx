a51a27a162125ba5c84cb73c0bfd9d93cee3f9ce
lavi shiratzky <lavi,shiratzky@gmail.com>
2023-03-04 22:49:23 +0200

    social media and fonts

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import "./Footer.css";
 import {FaFacebookF  } from "react-icons/fa";
+import Sochialmedia from "../Sochialmedia/Sochialmedia";
 function Footer(): JSX.Element {
     return (
         <div className="Footer">
-			<p>all rights reserved to Lavi Shiratzky &copy;<FaFacebookF/></p>
+			<p>all rights reserved to Lavi Shiratzky &copy;</p>
+            <Sochialmedia/>
             
         </div>
     );
---
+++ src/Components/Sochialmedia/Sochialmedia.tsx
@@ -0,0 +1,13 @@
+import "./Sochialmedia.css";
+import {FaFacebookF ,FaAccessibleIcon,FaTelegramPlane  } from "react-icons/fa";
+function Sochialmedia(): JSX.Element {
+    return (
+        <div className="Sochialmedia">
+			<FaFacebookF/>
+           <FaAccessibleIcon/>
+           <FaTelegramPlane/>
+        </div>
+    );
+}
+
+export default Sochialmedia;
