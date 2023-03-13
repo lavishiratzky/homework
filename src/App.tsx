0bf9180b71dd49f236ce043fbc8b1f82bbcb6e2f
lavi shiratzky <lavi,shiratzky@gmail.com>
2023-03-13 23:18:01 +0200

    dark and light mode , and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
@@ -7,8 +7,18 @@ import Footer from './Components/Footer/Footer';
 import Menu from './Components/Menu/Menu';
 import Routing from './Components/Routing/Routing';
 function App(): JSX.Element {
+  const [theme, setTheme] = useState<string>('light')
+  const changeTheme =() =>{
+    if  (theme =='light'){
+      setTheme('dark');
+    }
+    else{
+      setTheme('light');
+    }
+  }
   return (
-    <div className="App">
+    <div className={"App ${theme}"}>
+      <button onClick={changeTheme}>{ theme==='dark'? 'MdLightMode' : 'MdDarkMode' }</button>
       <Header/>
       <Menu/>
       <Main/>
